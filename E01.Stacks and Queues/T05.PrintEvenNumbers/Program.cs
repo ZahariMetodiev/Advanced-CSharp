68b3ae7a3a62a5e62927fd49853264e4757d7b8a
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-19 10:30:06 +0300

    Add task PrintEvenNumbers

---
+++ E01.Stacks and Queues/T05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> numbersQueue = new Queue<int>(numbers);
+            Queue<int> evenNumbers= new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    evenNumbers.Enqueue(currentNumber);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", evenNumbers));
+        }
+    }
+}
