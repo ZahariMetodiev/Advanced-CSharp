332c9349e747f45bf677985913b2096deb37d3e2
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-19 10:30:26 +0300

    Add task Supermarket

---
+++ E01.Stacks and Queues/T06.Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
