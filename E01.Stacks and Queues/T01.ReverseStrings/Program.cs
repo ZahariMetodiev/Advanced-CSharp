ca9200e7be57e82c6a0b0fb11f7f8339b35549ae
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-19 10:28:34 +0300

    Add task ReverseString

---
+++ E01.Stacks and Queues/T01.ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> output = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output.Push(input[i]);
+            }
+
+            while (output.Count > 0)
+            {
+                Console.Write(output.Pop());
+            }
+        }
+    }
+}
