84f4a2d25e38fdc5795fba57cf851d265b9cf34f
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-19 10:31:28 +0300

    Add task TrafficJam

---
+++ E01.Stacks and Queues/T08.TrafficJam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> cars = new Queue<string>();
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            counter++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
