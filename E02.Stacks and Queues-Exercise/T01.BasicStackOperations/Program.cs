5ea83a2ce107fc6d94442cd6d89c4a3ef5dbe3a8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-20 15:01:47 +0300

    Add task BasicStackOperations

---
+++ E02.Stacks and Queues-Exercise/T01.BasicStackOperations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            int push = command[0];
+            int pop = command[1];
+            int check = command[2];
+
+            for (int i = 0; i < push; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+            for (int i = 0; i < pop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (stack.Contains(check))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
