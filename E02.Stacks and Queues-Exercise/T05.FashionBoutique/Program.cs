30948ac9274d512cf26ae2fbca41a42030db0d1b
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-20 15:03:57 +0300

    Add task FashionBoutique

---
+++ E02.Stacks and Queues-Exercise/T05.FashionBoutique/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            int racks = 1;
+            int currCapacity = capacity;
+
+            Stack<int> clothes = new Stack<int>(input);
+
+            while (clothes.Count > 0)
+            {
+                int currClothes = clothes.Peek();
+
+                if (currCapacity >= currClothes)
+                {
+                    currCapacity -= currClothes;
+                    clothes.Pop();
+                }
+                else
+                {
+                    racks++;
+                    currCapacity = capacity;
+                }
+            }
+            Console.WriteLine(racks);
+        }
+    }
+}
