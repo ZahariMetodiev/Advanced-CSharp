fb51307c2a0bc6ba2ef004161b18d49a5a31bfa1
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-21 11:36:37 +0300

    Add task SymbolInMatrix

---
+++ E03.Multidimensional Arrays/T04.SymbolInMatrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace T04.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string input = Console.ReadLine();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            bool isFind = true;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == symbol)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        return;
+                    }
+                    else
+                    {
+                        isFind = false;
+                    }
+                }
+            }
+
+            if (!isFind)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
