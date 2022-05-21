15e82280556cc9b7a3bf39217d44805719083659
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-21 11:36:13 +0300

    Add task PrimaryDiagonal

---
+++ E03.Multidimensional Arrays/T03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace T03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size,size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[row, cols] = input[cols];
+                }
+            }
+
+            int diagonalSum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                diagonalSum += matrix[i, i];
+            }
+
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}
