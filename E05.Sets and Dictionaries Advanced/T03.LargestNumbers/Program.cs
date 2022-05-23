b49219ca83497bebb9f19deded47571b8ff91525
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-23 08:21:04 +0300

    Add task LargestNumbers

---
+++ E05.Sets and Dictionaries Advanced/T03.LargestNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace T03.LargestNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sortedNumbers = numbers.OrderByDescending(x => x).ToArray();
+
+            if (sortedNumbers.Length <= 3)
+            {
+                for (int i = 0; i < sortedNumbers.Length; i++)
+                {
+                    Console.Write(sortedNumbers[i] + " ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(sortedNumbers[i] + " ");
+                }
+            }
+
+        }
+    }
+}
