81f240b3e1f852ec2d0348094cff3b990e0c4d7a
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-23 08:22:31 +0300

    Add task ParkingLot

---
+++ E05.Sets and Dictionaries Advanced/T07.ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+                string number = tokens[1];
+
+                if (command == "IN")
+                {
+                    carNumbers.Add(number);
+                }
+                else if (command == "OUT")
+                {
+                    carNumbers.Remove(number);
+                }
+            }
+
+            if (carNumbers.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, carNumbers));
+            }
+        }
+    }
+}
