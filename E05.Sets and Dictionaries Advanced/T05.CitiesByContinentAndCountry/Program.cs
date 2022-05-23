6c0e8345f6e53fc2324c13799587fbff58a9e0b2
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-23 08:21:49 +0300

    Add task CitiesByContinentAndCountry

---
+++ E05.Sets and Dictionaries Advanced/T05.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T05.CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, string>> cities = new Dictionary<string, Dictionary<string, string>>();
+
+            
+        }
+    }
+}
