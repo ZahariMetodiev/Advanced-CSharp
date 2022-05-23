b1d8596068a8757fa7d31875aeb2d86f38ffaf2c
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-23 08:19:39 +0300

    Add task CountSameValuesInArray

---
+++ E05.Sets and Dictionaries Advanced/T01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double currentNumber = input[i];
+
+                if (!numbers.ContainsKey(currentNumber))
+                {
+                    numbers.Add(currentNumber, 0);
+                }
+
+                numbers[currentNumber]++;
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
