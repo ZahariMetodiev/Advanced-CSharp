42f369f9f54fffa3d174a826e6127c4c35f15bcb
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-13 16:40:46 +0300

    Add task SetOfElements

---
+++ E06.Sets and Dictionaries Advanced-Exercise/T02.SetsOfElements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstSetLenght = input[0];
+            int secontSetLenght = input[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            int totalLenghts = firstSetLenght + secontSetLenght;
+
+            for (int i = 0; i < totalLenghts; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (i <= firstSetLenght)
+                {
+                    firstSet.Add(currentNumber);
+                }
+                else
+                {
+                    secondSet.Add(currentNumber);
+                }
+            }
+
+            int maxLenght = Math.Max(firstSet.Count, secondSet.Count);
+
+            for (int i = 0; i < maxLenght; i++)
+            {
+                if ()
+                {
+
+                }
+            }
+        }
+    }
+}
