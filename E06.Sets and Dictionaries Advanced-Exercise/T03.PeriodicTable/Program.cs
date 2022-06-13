61fdbd5db8e7de23b3854f359acf3aef738ddec6
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-13 16:41:04 +0300

    Add task PeriodicTable

---
+++ E06.Sets and Dictionaries Advanced-Exercise/T03.PeriodicTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in input)
+                {
+                    elements.Add(item);
+                }
+            }
+
+            foreach (var element in elements)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
